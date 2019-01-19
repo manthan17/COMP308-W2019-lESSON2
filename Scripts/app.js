40766390375bbadd85dc9fa8cb59dab7d8c1264f
Manthan Shah <manthanshah1711@gmail.com>
2019-01-19 10:40:55 -0800

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// Self-Executing Anonymous Function
+// IIFE - Immmediately Invoked Function Expression
+(function () {
+    function Start() {
+        console.log("%cApp Started... ", "font-size: 20px; color: blue;");
+    }
+    window.addEventListener("load", Start);
+})();
+//# sourceMappingURL=app.js.map
